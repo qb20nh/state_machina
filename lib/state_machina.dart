f84b74274c86f01f993a6abc3f44e2cc8474f350
Taejong Yoo <yut951121@gmail.com>
2023-08-03 04:19:57 +0900

    refactor: generics and more type safety stuff
    - parameterize both the state and event types
    - use `final` for immutability
    - refrain from using `dynamic`

--- lib/state_machina.dart
+++ lib/state_machina.dart
@@ -1,22 +1,23 @@
+typedef StateTransitionMap<S, E> = Map<S, Map<E, S>>;
+
 /// A simple state machine class that allows easily creating a FSM based on a state map and optional initial state, validating the correctness of both, and handles events, updating the current state per the state map.
-class StateMachine {
+class StateMachine<S, E> {
   /// The current state
-  dynamic current;
+  late S current;
 
-  Map<dynamic, Map<dynamic, dynamic>> _stateMap = Map();
-  Set<dynamic> _stateKeys = Set();
-  Set<dynamic> _stateValues = Set();
-  Set<dynamic> _eventIds = Set();
+  late final StateTransitionMap<S, E> _stateMap;
+  final Set<S> _stateKeys = {};
+  final Set<S> _stateValues = {};
+  final Set<E> _eventIds = {};
 
-  List<Function> _listeners = [];
+  final List<Function> _listeners = [];
 
   /// Returns a finite state machine based on [stateMap] and optional [initialState].
   ///
   /// Throws [ArgumentError] if the [stateMap] is not valid (unreachable or non-existent states).
   /// Throws [ArgumentError] if [initialState] is not found in [stateMap].
-  StateMachine(Map<dynamic, Map<dynamic, dynamic>> stateMap,
-      [dynamic initialState]) {
-    for (MapEntry<dynamic, Map<dynamic, dynamic>> state in stateMap.entries) {
+  StateMachine(StateTransitionMap<S, E> stateMap, [S? initialState]) {
+    for (final state in stateMap.entries) {
       _stateKeys.add(state.key);
       _stateValues.addAll(state.value.values);
       _eventIds.addAll(state.value.keys);
@@ -34,14 +35,14 @@ class StateMachine {
       current = initialState;
     }
 
-    for (var stateName in _stateKeys.skip(1)) {
+    for (final stateName in _stateKeys.skip(1)) {
       if (!_stateValues.contains(stateName)) {
         throw ArgumentError(
             'State $stateName cannot be entered since it is not the first entry in the state map, and it does not occur as a next state for any other state.');
       }
     }
 
-    for (dynamic stateName in _stateValues) {
+    for (final stateName in _stateValues) {
       if (!_stateKeys.contains(stateName)) {
         throw ArgumentError(
             'State $stateName is used as a next state in stateMap but does not exist as a key in stateMap.');
@@ -52,33 +53,31 @@ class StateMachine {
   /// Sends [eventId] into the state machine and optionally transitions to the next state based on the state map.
   ///
   /// Throws [ArgumentError] if [eventId] is not found in [stateMap].
-  void send(dynamic eventId) {
+  void send(final eventId) {
     if (!_eventIds.contains(eventId)) {
       throw ArgumentError(
           'Received an unknown event: $eventId. Only events present in the state map may be sent.');
     }
 
-    dynamic previous = current;
+    final previous = current;
 
     if (_stateMap[current]!.isNotEmpty) {
-      current = _stateMap[current]![eventId];
+      current = _stateMap[current]![eventId]!;
     }
 
-    for (var func in _listeners) {
+    for (final func in _listeners) {
       func(current, previous, eventId);
     }
   }
 
   /// Register a listener to be called after each send is resolved
   /// Listener is called with listener(current, previous, event)
-  void addListener(
-      Function(dynamic current, dynamic previous, dynamic event) listener) {
+  void addListener(Function(S current, S previous, E event) listener) {
     _listeners.add(listener);
   }
 
   /// Remove a previously registered closure from the list of listeners
-  void removeListener(
-      Function(dynamic current, dynamic previous, dynamic event) listener) {
+  void removeListener(Function(S current, S previous, E event) listener) {
     _listeners.remove(listener);
   }
 }
